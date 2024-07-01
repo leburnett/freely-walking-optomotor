41839e5da4aa9854020d6c9bbe2b9a0d72125716
leburnett <leburnett3@gmail.com>
2024-07-01 10:47:36 -0400

    Create run_core_functions.m

---
+++ run_core_functions.m
@@ -0,0 +1,21 @@
+% Script to run functions from for freely walking optotmotor experiments
+
+path_to_folder = '/Users/burnettl/Documents/Janelia/HMS_2024/DATA/2024_06_17';
+save_figs = false;
+genotype = 'csw1118';
+
+process_freely_walking_optomotor(path_to_folder, save_figs, genotype)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
