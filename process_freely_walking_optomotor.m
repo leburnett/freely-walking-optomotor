dfca789a3e51e827a9f4b4cd19fc36ceb34853ea
Hannah-Marie Santos <hmsantos@unc.edu>
2024-07-09 15:27:15 -0400

    Added the velocity datapoints and lip plot stuff

--- process_freely_walking_optomotor.m
+++ process_freely_walking_optomotor.m
@@ -124,9 +124,9 @@ function process_freely_walking_optomotor(path_to_folder, save_figs, genotype)
        
         plot_ang_vel_per_fly(Log, trx, n_flies, n_conditions, fps, title_str, save_str, fig_exp_save_folder, save_figs)
     
-        %% Make 'datapoints' 
+        %% Make 'ang datapoints' 
     
-        datapoints = make_mean_ang_vel_datapoints(Log, trx, n_flies, n_conditions, fps);
+        ang_datapoints = make_mean_ang_vel_datapoints(Log, trx, n_flies, n_conditions, fps);
     
         %% Plot the mean ang velocity per condition for all flies as scatter points. "Fish plot" 
        
@@ -134,7 +134,7 @@ function process_freely_walking_optomotor(path_to_folder, save_figs, genotype)
         % until the first flicker, then use "data_to_use = datapoints(1:17, :)" 
         % else use "data_to_use = datapoints". 
         
-        data_to_use = datapoints; %(1:17, :);
+        data_to_use = ang_datapoints; %(1:17, :);
     
         % Update contrast values for acclim / flickers for plotting:
         % OFF ACCLIM
@@ -155,14 +155,49 @@ function process_freely_walking_optomotor(path_to_folder, save_figs, genotype)
         % Individual flies in light pink/blue. 
         % Average across flies in bold.
         
-        data_to_use = datapoints; %(1:17, :);
+        data_to_use = ang_datapoints; %(1:17, :);
        
         plot_line_ang_vel_all_flies(data_to_use, n_flies, title_str, save_str, fig_exp_save_folder, save_figs)
     
+        %% Make 'vel datapoints' 
+    
+        vel_datapoints = make_mean_ang_vel_datapoints(Log, trx, n_flies, n_conditions, fps);
+    
+        %% Plot the mean velocity per condition for all flies as scatter points. "Fish plot" 
+       
+        % If you only want to plot the data from the conditions ramping up, up
+        % until the first flicker, then use "data_to_use = datapoints(1:17, :)" 
+        % else use "data_to_use = datapoints". 
+        
+        data_to_use = vel_datapoints; %(1:17, :);
+    
+        % Update contrast values for acclim / flickers for plotting:
+        % OFF ACCLIM
+        data_to_use(1,1) = -0.2;
+        % ON ACCLIM 
+        data_to_use(2,1) = -0.1;
+        % FLICKER 1
+        data_to_use(17,1) = 1.2;
+        % FLICKER 2 
+        data_to_use(32,1) = 1.3;
+        % OFF ACCLIM 2
+        data_to_use(33,1) = 1.4;
+        
+        plot_scatter_vel_all_flies(data_to_use, n_flies, title_str, save_str, fig_exp_save_folder, save_figs)
+    
+        %% Generate a line plot for mean vel at each contrast level. "Lips plot"
+        
+        % Individual flies in light pink/blue. 
+        % Average across flies in bold.
+        
+        data_to_use = vel_datapoints; %(1:17, :);
+       
+        plot_line_vel_all_flies(data_to_use, n_flies, title_str, save_str, fig_exp_save_folder, save_figs)
+        
         %% SAVE
         
         % save data
-        save(fullfile(data_save_folder, strcat(save_str, '_data.mat')), 'datapoints', 'data_to_use', 'Log', 'trx');
+        save(fullfile(data_save_folder, strcat(save_str, '_data.mat')), 'vel_datapoints', 'data_to_use', 'Log', 'trx');
         
     end 
 
