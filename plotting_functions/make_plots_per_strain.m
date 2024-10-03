bb5b3a112d49fc64ca6ea21f28b288600d89a83a
leburnett <leburnett3@gmail.com>
2024-10-03 10:17:04 -0400

    Update make_plots_per_strain.m

--- plotting_functions/make_plots_per_strain.m
+++ plotting_functions/make_plots_per_strain.m
@@ -1,6 +1,7 @@
 % Functions to analyse the behaviour of strains across all experiments
 
 strain_data_folder = '/Users/burnettl/Documents/Projects/oaky_cokey/results/protocol_v1/JFRC49_ES';
+cd(strain_data_folder)
 
 data_files = dir('*.mat');
 n_files = length(data_files);
@@ -25,6 +26,7 @@ for i = 1:n_files
 
 end 
 
+%% Plots
 
 figure; plot(median(all_velocity_data))
 
@@ -44,4 +46,58 @@ end
 
 figure; histogram(p_slow, 'BinEdges', [0:0.05:1])
 
-% Do a more specific version of this just during the turning too. 
+% Histogram of velocity over acclim for all exp
+figure; histogram(all_velocity_data(:, 1:900), 'BinEdges', [0:2:50])
+
+
+%% distance to wall data 
+
+all_distance_data = []; 
+
+for i = 1:n_files
+    load(data_files(i).name);
+    dist_data = 120-feat.data(:, :, 9);
+
+    [rowsall, colsall] = size(all_distance_data);
+    [rowsdata, colsdata]  = size(dist_data);
+
+    n_cols_diff = colsall-colsdata;
+    if n_cols_diff >0 
+        dist_data = [dist_data, NaN(rowsdata, n_cols_diff)];
+    elseif n_cols_diff < 0 
+        all_distance_data = [all_distance_data, NaN(rowsall, n_cols_diff*-1)];
+    end 
+
+    all_distance_data = vertcat(all_distance_data, dist_data);
+
+end 
+
+%% Plots
+
+figure; plot(median(all_distance_data))
+
+figure; plot(median(all_distance_data))
+
+% Histogram of velocity over experiments
+figure; histogram(all_distance_data, 'BinEdges', [0:2:50])
+
+
+% Histogram of velocity over acclim for all exp
+figure; histogram(all_distance_data(:, 1:900), 'BinEdges', [0:2:50])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
