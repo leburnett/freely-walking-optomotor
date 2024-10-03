2c4c80f07209401361ddecf21eeae31b33740cfb
leburnett <leburnett3@gmail.com>
2024-10-03 11:50:17 -0400

    Update process_freely_walking_data.m
    Convert into function

--- processing_functions/process_freely_walking_data.m
+++ processing_functions/process_freely_walking_data.m
@@ -1,47 +1,49 @@
+function process_freely_walking_data(date_to_analyse)
 % process the oaky-cokey freely-walking optomotor behaviour data 
 
-close all
-clear
-clc
-
-%% If data recorded after 24/09/2024 - - - new logging / saving system that saves in subfolders. 
-PROJECT_ROOT = '/Users/burnettl/Documents/Projects/oaky_cokey/'; %% Update for your computer. 
-data_path = fullfile(PROJECT_ROOT, 'data');
-results_path = fullfile(PROJECT_ROOT, 'results');
-
-date_to_analyse = '2024_09_27';
-
-date_folder = fullfile(data_path, date_to_analyse);
-
-cd(date_folder)
-
-protocol_folders = dir('*rotocol_*');
-n_protocols = height(protocol_folders);
-
-for proto_idx = 1:n_protocols
-
-    protocol_to_analyse = protocol_folders(proto_idx).name;
-    cd(fullfile(protocol_folders(proto_idx).folder, protocol_folders(proto_idx).name))
-
-    strain_folders = dir();
-    % make sure only appropriate folders are considered.
-    strain_folders = strain_folders(3:end, :);
-    strain_names = {strain_folders.name};
-    strain_folders = strain_folders(~strcmp(strain_names, '.DS_Store'));
-    n_strains = length(strain_folders);
-
-    for strain_idx = 1:n_strains
-        genotype_to_analyse =  strain_folders(strain_idx).name;
-        
-        path_to_folder = fullfile(data_path, date_to_analyse, protocol_to_analyse, genotype_to_analyse);
-        save_folder = fullfile(results_path, protocol_to_analyse, genotype_to_analyse);
-        
-        % Process the data and save the processed data:
-        process_data_features(path_to_folder, save_folder, date_to_analyse)
+    close all
+    clear
+    clc
+    
+    %% If data recorded after 24/09/2024 - - - new logging / saving system that saves in subfolders. 
+    PROJECT_ROOT = '/Users/burnettl/Documents/Projects/oaky_cokey/'; %% Update for your computer. 
+    data_path = fullfile(PROJECT_ROOT, 'data');
+    results_path = fullfile(PROJECT_ROOT, 'results');
+    
+    % date_to_analyse = '2024_09_27';
+    
+    date_folder = fullfile(data_path, date_to_analyse);
+    
+    cd(date_folder)
+    
+    protocol_folders = dir('*rotocol_*');
+    n_protocols = height(protocol_folders);
+    
+    for proto_idx = 1:n_protocols
+    
+        protocol_to_analyse = protocol_folders(proto_idx).name;
+        cd(fullfile(protocol_folders(proto_idx).folder, protocol_folders(proto_idx).name))
+    
+        strain_folders = dir();
+        % make sure only appropriate folders are considered.
+        strain_folders = strain_folders(3:end, :);
+        strain_names = {strain_folders.name};
+        strain_folders = strain_folders(~strcmp(strain_names, '.DS_Store'));
+        n_strains = length(strain_folders);
+    
+        for strain_idx = 1:n_strains
+            genotype_to_analyse =  strain_folders(strain_idx).name;
+            
+            path_to_folder = fullfile(data_path, date_to_analyse, protocol_to_analyse, genotype_to_analyse);
+            save_folder = fullfile(results_path, protocol_to_analyse, genotype_to_analyse);
+            
+            % Process the data and save the processed data:
+            process_data_features(path_to_folder, save_folder, date_to_analyse)
+        end 
+    
     end 
 
 end 
-
 %% If data recorded on or before 24/09/2024 - - - all experiments saved in one date folder. 
 
 % What date to analyse:
