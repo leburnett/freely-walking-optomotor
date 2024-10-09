1d4772ec1eb049fe3769245afb526a1108dc1e04
leburnett <leburnett3@gmail.com>
2024-10-09 09:53:36 -0400

    Find frames when transitions happen

--- plotting_functions/functions/plot_trajectory.m
+++ plotting_functions/functions/plot_trajectory.m
@@ -1,5 +1,10 @@
 
 % Plot trajectories. 
+% What is important to plot?
+% Quantify whether they turn in out of circle. 
+% Horsetail plot. centre on position at transition. Take away those xy
+% values from subsequent ones. Plots them with different time intervals. 
+
 
 x = trx.x;
 y = trx.y;
@@ -66,33 +71,80 @@ axis square
 
 %%
 
+t_len = 5; % Length of time in s to plot the 'tail' of the trajectory
 
-figure
-for i= 12410:12714
-    x1 = x(i);
-    x2 = x(i+1);
-    y1 = y(i);
-    y2 = y(i+1);
-    if ismember(i, frames_blue)
-        col = 'b';
-    elseif ismember(i, frames_pink)
-        col = 'm';
-    else 
-        col = 'k';
+
+% % %  1 to -1 transitions
+
+% Find the last frame of the '1' dir stimulus. 
+rows1 = find(Log.dir == 1);
+
+frames_transition1 = [];
+for j = 1:numel(rows1)
+    row = rows1(j);
+
+    if Log.dir(row+1) == -1
+        f_stop1 = Log.stop_f(row);
+        f_start_1 = Log.start_f(row+1);
+        if f_stop1 ~= f_start_1
+            val = ceil(mean([f_stop1, f_start_1]));
+        else 
+            val = f_stop1;
+        end 
+        frames_transition1 = [frames_transition1, val];
     end 
-    plot([x1, x2], [y1 y2], col, 'LineWidth', 1)
-    hold on 
 end 
-%Start position
-% plot(x(1), y(1), 'r.', 'MarkerSize', 20)
-% End position
-% plot(x(end), y(end), 'b.', 'MarkerSize', 20)
-viscircles([512, 512], 500, 'Color', [0.7 0.7 0.7])
-xlim([0 1024])
-ylim([0 1024])
-axis off
-axis square
-title('Flicker')
+
+% % %  -1 to 1 transitions
+
+rows_1 = find(Log.dir == -1);
+
+frames_transition2 = [];
+frames_transition3 = [];
+
+for j = 1:numel(rows_1)
+    row = rows_1(j);
+
+    if Log.dir(row+1) == 1 % Goes back to '1' dir
+
+        f_stop_1 = Log.stop_f(row);
+        f_start1 = Log.start_f(row+1);
+        if f_stop_1 ~= f_start1
+            val = ceil(mean([f_stop_1, f_start1]));
+        else 
+            val = f_stop_1;
+        end 
+        frames_transition2 = [frames_transition2, val];
+
+    elseif Log.dir(row+1) ~= 1 % Goes on to Flicker next.
+
+        f_stop_1 = Log.stop_f(row);
+        f_start1 = Log.start_f(row+1);
+        if f_stop_1 ~= f_start1
+            val = ceil(mean([f_stop_1, f_start1]));
+        else 
+            val = f_stop_1;
+        end 
+        frames_transition3 = [frames_transition3, val];
+
+    end 
+end 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
