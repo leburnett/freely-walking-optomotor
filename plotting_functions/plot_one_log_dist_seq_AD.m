00b915b9b73ef0ae915d161215872d3442f23171
adev97 <aparnadev97@gmail.com>
2024-10-29 16:01:50 -0400

    add blue lines for the start of each stimulus

--- plotting_functions/plot_one_log_dist_seq_AD.m
+++ plotting_functions/plot_one_log_dist_seq_AD.m
@@ -10,6 +10,7 @@ function plot_one_log_dist_seq_AD(current_log, feat, trx, title_str)
 % use for testing
 % current_log = LOG.log_4;
 % title_str = 'log_4';
+% figure;
 
 % fixed_parameters
 n_flies = length(trx);
@@ -19,8 +20,6 @@ samp_rate = 1/fps;
 method = 'line_fit';
 t_window = 16;
 cutoff = [];
-
-% figure;
 
 % plot the log's information in one log that you will bring into a subplot
 % later
@@ -34,13 +33,17 @@ flicker_start = max(current_log.start_f) - current_start_f;
 
 dist_data = feat.data(:, current_start_f:current_stop_f, 9);
 dist_data = 120-dist_data;
-% subplot(4, 1, 4)
 
 n_conditions = size(current_log.start_t, 2);
 
-
-
 hold on
+
+% add blue lines at each start and stop point for the conditions
+for i = 1:n_conditions - 1
+    temp_start_f = current_log.start_f(i) - current_start_f;
+    xline(temp_start_f, '-b', 'LineWidth', 1);
+end
+
 for idx = 1:n_flies
   % Plot the distance from the centre per fly
     plot(dist_data(idx, :), 'Color', [0.7 0.7 0.7], 'LineWidth', 0.5)
@@ -55,7 +58,8 @@ plot(mean(dist_data), 'k', 'LineWidth', 1)
 title(title_str);
 
 xline(flicker_start, '--r', 'LineWidth', 1);
-
+xlabel('time (s)')
+ylabel('distance (mm)')
 
 hold off
 
