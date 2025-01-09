b4aac62d7c8745d938ea34522d01913e4a4b9495
leburnett <leburnett3@gmail.com>
2025-01-09 16:05:48 -0500

    add curv and forward vel to summary plot

--- plotting_functions/functions/plot_all_features_filt.m
+++ plotting_functions/functions/plot_all_features_filt.m
@@ -21,52 +21,32 @@ function f = plot_all_features_filt(LOG, comb_data, protocol, title_str)
     n_flies = length(trx);
 
     figure
-    % % % % % % % % Subplot 1 = HEADING % % % % % % % % %
 
-    heading_data = comb_data.heading_data;
-    ang_vel_data = comb_data.av_data;
+    % % % % % % % % Subplot 1 = VELOCITY % % % % % % % % %
+    velocity_data = comb_data.fv_data;
+    xmax = size(velocity_data, 2);
 
     subplot(4, 1, 1)
-
-    max_val = max(max(heading_data));
-    min_val = min(min(heading_data));
-    xmax = size(heading_data, 2);
-    
-    % Plot the boundaries between when the stimulus changes.
-    plot_pink_blue_rects(LOG, protocol, min_val, max_val)
-
-    for idx = 1:n_flies
-      % Plot the heading angle per fly
-        plot(heading_data(idx, :), 'Color', [0.7 0.7 0.7], 'LineWidth', 1)
-    end 
-
-    % Plot the heading angle
-    plot(mean(heading_data), 'k', 'LineWidth', 2.5)
-    ylabel('Heading (deg)')
-    xlim([0 xmax])
-
-    % % % % % % % % Subplot 2 = VELOCITY % % % % % % % % %
-
-    velocity_data = comb_data.fv_data;
-    
-    subplot(4, 1, 2)
    
     % Plot the boundaries between when the stimulus changes.
     plot_pink_blue_rects(LOG, protocol, -2, 50)
 
     for idx = 1:n_flies
       % Plot the velocity per fly
-        plot(velocity_data(idx, :), 'Color', [0.7 0.7 0.7], 'LineWidth', 1)
+        plot(velocity_data(idx, :), 'Color', [0.7 0.7 0.7], 'LineWidth', 0.2)
     end 
 
     % Plot the velocity
-    plot(mean(velocity_data), 'k', 'LineWidth', 2.5)
-    ylabel('Velocity (mm s-1)')
+    plot(mean(velocity_data), 'w', 'LineWidth', 1.25)
+    plot(smoothdata(mean(velocity_data), 'lowess'), 'k', 'LineWidth', 1.25)
+    ylabel('Forward velocity (mm s-1)')
     % ylim([-2 50])
     xlim([0 xmax])
-    % % % % % % % % Subplot 3 = ANG VEL % % % % % % % % %
+    % % % % % % % % Subplot 2 = ANG VEL % % % % % % % % %
 
-    subplot(4, 1, 3)
+    subplot(4, 1, 2)
+    
+    ang_vel_data = comb_data.av_data;
 
     max_val = prctile(ang_vel_data(:), 99.5);
     min_val = prctile(ang_vel_data(:), 0.5);
@@ -76,12 +56,36 @@ function f = plot_all_features_filt(LOG, comb_data, protocol, title_str)
 
     for idx = 1:n_flies
       % Plot the ang vel per fly
-        plot(ang_vel_data(idx, :), 'Color', [0.7 0.7 0.7], 'LineWidth', 1)
+        plot(ang_vel_data(idx, :), 'Color', [0.7 0.7 0.7], 'LineWidth', 0.2)
     end 
 
     % Plot the ang vel
-    plot(mean(ang_vel_data), 'k', 'LineWidth', 2.5)
+    plot(mean(ang_vel_data), 'w', 'LineWidth', 1.25)
+    plot(smoothdata(mean(ang_vel_data), 'lowess'), 'k', 'LineWidth', 1.5)
     ylabel('Angular velocity (deg s-1)')
+    xlim([0 xmax])
+
+    % % % % % % % Subplot 3 = CURVATURE / Turning rate % % % % % % % % %
+
+    curv_data = comb_data.curv_data;
+
+    subplot(4, 1, 3)
+
+    max_val = prctile(curv_data(:), 99.5);
+    min_val = prctile(curv_data(:), 0.5);
+    
+    % Plot the boundaries between when the stimulus changes.
+    plot_pink_blue_rects(LOG, protocol, min_val, max_val)
+
+    for idx = 1:n_flies
+        plot(curv_data(idx, :), 'Color', [0.7 0.7 0.7], 'LineWidth', 0.5)
+    end 
+
+    % Plot 
+    plot(mean(curv_data), 'w', 'LineWidth', 1.25)
+    % plot(smoothdata(mean(curv_data), 'lowess'), 'k', 'LineWidth', 1)
+    plot(smoothdata(mean(curv_data), 'movmedian'), 'k', 'LineWidth', 1)
+    ylabel('Curvature (deg/mm)')
     xlim([0 xmax])
 
     % % % % % % % % Subplot 4 = DISTANCE FROM CENTRE % % % % % % % % %
@@ -105,14 +109,15 @@ function f = plot_all_features_filt(LOG, comb_data, protocol, title_str)
     plot([0 xmax], [0 0], 'LineWidth', 1, 'Color', [0 0 0])
 
     % Plot the distance from the centre
-    plot(mean(dist_data), 'k', 'LineWidth', 2.5)
+    plot(mean(dist_data), 'Color', 'k', 'LineWidth', 1.5)
+    % plot(smoothdata(mean(dist_data), 'lowess'), 'k', 'LineWidth', 1.5)
     ylabel('Distance from the centre (mm)')
     xlim([0 xmax])
 
     title_str = strrep(title_str, '_', '-');
     sgtitle(strcat(title_str, ' - N=', string(n_flies)))
     f = gcf; 
-    f.Position = [1234  71  567  976];
+    f.Position = [6  289  1321  757]; %[1234  71  567  976];
     han=axes(f, 'visible','off');
     han.XLabel.Visible='on';
     xlabel(han, 'Time / frames / conditions')
