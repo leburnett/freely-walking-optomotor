215a2e236017a16a436dd75d1bf0dcb8022ca647
leburnett <leburnett3@gmail.com>
2025-03-19 17:03:05 -0400

    Update gaussian_conv.m

--- processing_functions/functions/gaussian_conv.m
+++ processing_functions/functions/gaussian_conv.m
@@ -1,11 +1,11 @@
 function x_smooth = gaussian_conv(x)
 % x = 1D array to be smoothed.
 % window_size = how many points in the kernel. 
-
+    n = numel(x);
     window_size = 15;
     sigma = 1;  % Standard deviation of Gaussian
     gauss_kernel = fspecial('gaussian', [1, window_size], sigma);
     
     x_smooth = x;
-    x_smooth((window_size/2)+1:n-(window_size/2)) = conv(x, gauss_kernel, 'valid');  % Apply Gaussian smoothing
+    x_smooth(floor(window_size/2)+1:n-floor(window_size/2)) = conv(x, gauss_kernel, 'valid');  % Apply Gaussian smoothing
 end 
