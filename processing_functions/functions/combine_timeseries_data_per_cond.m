1ae797c96103c158dc7627031ce10152addf1433
leburnett <leburnett3@gmail.com>
2025-03-26 09:03:52 -0400

    update generation of cond_data to incl. acclim

--- processing_functions/functions/combine_timeseries_data_per_cond.m
+++ processing_functions/functions/combine_timeseries_data_per_cond.m
@@ -8,6 +8,9 @@ function cond_data = combine_timeseries_data_per_cond( ...
 % DATA is generated using 'comb_data_across_cohorts_cond' from the results
 % files stored within 'oaky-cokey/results/protocol_X' folder.
 
+% If 'cond_idx' == 0, then it combines the data during the acclimatisation
+% period at the beginning. 
+
 % Returns:
 % ________
 % cond_data : array (size = [n_flies, n_frames_condition+interval])
@@ -15,77 +18,103 @@ function cond_data = combine_timeseries_data_per_cond( ...
     data = DATA.(strain).(sex); 
     n_exp = length(data); % Number of experiments run for this strain / sex.
 
-    idx2 = cond_idx;
-    
-    rep1_str = strcat('R1_condition_', string(idx2));   
-    rep2_str = strcat('R2_condition_', string(idx2));  
-    
-    % contains one row per fly and rep - combines data across all flies and all reps.
-    cond_data = []; 
-    nf_comb = size(cond_data, 2);
-    
-    for idx = 1:n_exp
-        % disp(idx)
-    
-        rep1_data = data(idx).(rep1_str);
-    
-        if ~isempty(rep1_data) % check that the row is not empty.
-    
-            % Extract the relevant data
-            rep1_data = rep1_data.(data_type);
-            rep2_data = data(idx).(rep2_str).(data_type);
-    
-            % Number of frames in each rep
-            nf1 = size(rep1_data, 2);
-            nf2 = size(rep2_data, 2);
-    
-            if nf1>nf2
-                nf = nf2;
-            elseif nf2>nf1
-                nf = nf1;
-            else 
-                nf = nf1;
-            end 
-    
-            % Trim data to same length
-            rep1_data = rep1_data(:, 1:nf);
-            rep2_data = rep2_data(:, 1:nf);
-    
-            % Initialise empty array:
-            rep_data = zeros(size(rep1_data));
-    
+    if cond_idx == 0 % ACCLIM 1 - OFF 
+
+        cond_data = []; 
+        nf_comb = size(cond_data, 2);
+
+        for idx = 1:n_exp
+
+            acclim_data = data(idx).acclim_off1.(data_type);
             nf_comb = size(cond_data, 2);
-    
-            if idx == 1 || nf_comb == 0 % 
-    
-                for rr = 1:size(rep1_data, 1)
-                    rep_data(rr, :) = mean(vertcat(rep1_data(rr, :), rep2_data(rr, :)));
-                end 
-    
-            else
+            
+            if nf_comb ~= 0 % First exp
+                nf = size(acclim_data, 2);
+
                 if nf>nf_comb % trim incoming data
-                    rep1_data = rep1_data(:, 1:nf_comb);
-                    rep2_data = rep2_data(:, 1:nf_comb);
+                    acclim_data = acclim_data(:, 1:nf_comb);
                 elseif nf_comb>nf % Add NaNs to end
                     diff_f = nf_comb-nf+1;
-                    n_flies = size(rep1_data, 1);
-                    rep1_data(:, nf:nf_comb) = NaN(n_flies, diff_f);
-                    rep2_data(:, nf:nf_comb) = NaN(n_flies, diff_f);
+                    n_flies = size(acclim_data, 1);
+                    acclim_data(:, nf:nf_comb) = NaN(n_flies, diff_f);
                 end 
-                rep_data = zeros(size(rep1_data));
-                % For 'cond_data' have one row per fly - mean of 2
-                % reps - not one row per rep. 
-                for rr = 1:size(rep1_data, 1)
-                    rep_data(rr, :) = mean(vertcat(rep1_data(rr, :), rep2_data(rr, :)));
-                end 
-            end
-            % disp(height(rep_data))
-            cond_data = vertcat(cond_data, rep_data);
-    
+            end 
+            cond_data = vertcat(cond_data, acclim_data);
         end 
-    
-    end 
 
+    else
+        idx2 = cond_idx;
+        
+        rep1_str = strcat('R1_condition_', string(idx2));   
+        rep2_str = strcat('R2_condition_', string(idx2));  
+        
+        % contains one row per fly and rep - combines data across all flies and all reps.
+        cond_data = []; 
+        nf_comb = size(cond_data, 2);
+        
+        for idx = 1:n_exp
+            % disp(idx)
+        
+            rep1_data = data(idx).(rep1_str);
+        
+            if ~isempty(rep1_data) % check that the row is not empty.
+        
+                % Extract the relevant data
+                rep1_data = rep1_data.(data_type);
+                rep2_data = data(idx).(rep2_str).(data_type);
+        
+                % Number of frames in each rep
+                nf1 = size(rep1_data, 2);
+                nf2 = size(rep2_data, 2);
+        
+                if nf1>nf2
+                    nf = nf2;
+                elseif nf2>nf1
+                    nf = nf1;
+                else 
+                    nf = nf1;
+                end 
+        
+                % Trim data to same length
+                rep1_data = rep1_data(:, 1:nf);
+                rep2_data = rep2_data(:, 1:nf);
+        
+                % Initialise empty array:
+                rep_data = zeros(size(rep1_data));
+        
+                nf_comb = size(cond_data, 2);
+        
+                if idx == 1 || nf_comb == 0 % 
+        
+                    for rr = 1:size(rep1_data, 1)
+                        rep_data(rr, :) = mean(vertcat(rep1_data(rr, :), rep2_data(rr, :)));
+                    end 
+        
+                else
+                    if nf>nf_comb % trim incoming data
+                        rep1_data = rep1_data(:, 1:nf_comb);
+                        rep2_data = rep2_data(:, 1:nf_comb);
+                    elseif nf_comb>nf % Add NaNs to end
+                        diff_f = nf_comb-nf+1;
+                        n_flies = size(rep1_data, 1);
+                        rep1_data(:, nf:nf_comb) = NaN(n_flies, diff_f);
+                        rep2_data(:, nf:nf_comb) = NaN(n_flies, diff_f);
+                    end 
+                    rep_data = zeros(size(rep1_data));
+                    % For 'cond_data' have one row per fly - mean of 2
+                    % reps - not one row per rep. 
+                    for rr = 1:size(rep1_data, 1)
+                        rep_data(rr, :) = mean(vertcat(rep1_data(rr, :), rep2_data(rr, :)));
+                    end 
+                end
+                % disp(height(rep_data))
+                cond_data = vertcat(cond_data, rep_data);
+        
+            end 
+        
+        end 
+
+    end 
 end 
 
 
