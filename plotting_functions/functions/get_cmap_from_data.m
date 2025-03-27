c4b974705b8f01bf14bb778639aeabe96aae5514
leburnett <leburnett3@gmail.com>
2025-03-27 15:33:00 -0400

    Update get_cmap_from_data.m
    Set upper threshold. If values are above the cmax set, then set them to 1.

--- plotting_functions/functions/get_cmap_from_data.m
+++ plotting_functions/functions/get_cmap_from_data.m
@@ -4,7 +4,15 @@ function cmap_array = get_cmap_from_data(data)
     col_bin = data(:); % Ensure column vector
     col_bin = fillmissing(col_bin, 'nearest');
     col_bin_norm = (col_bin - min(col_bin)) / (max(col_bin) - min(col_bin));
-    % col_bin_norm = (col_bin - 0) / (125 - 0);
+
+    % turning rate
+    % col_bin_norm = (col_bin - 0) / (90 - 0);
+
+    % angular velocity 
+    % col_bin_norm = (col_bin - 0) / (220 - 0);
+
+    % Set any value above 120 to max.
+    col_bin_norm(col_bin_norm>1) = 1;
     
     % Define the colormap (e.g., 'parula', 'jet', 'viridis', etc.)
     cmap = parula(256); % Change colormap if needed
