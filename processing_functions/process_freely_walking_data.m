8ad820de2c5a02681815e5d5d94b32fce6a3d417
leburnett <leburnett3@gmail.com>
2025-03-28 15:37:18 -0400

    Update process_freely_walking_data.m

--- processing_functions/process_freely_walking_data.m
+++ processing_functions/process_freely_walking_data.m
@@ -1,17 +1,18 @@
-function process_freely_walking_data()
+function process_freely_walking_data(date_to_analyse)
 % process the oaky-cokey freely-walking optomotor behaviour data 
 
     close all
     clear
     clc
     
-    %% Enter date to be analysed here:
-    date_to_analyse = '2025_03_25';
+    % %% Enter date to be analysed here:
+    % date_to_analyse = '2025_03_25';
 
     %% If data recorded after 24/09/2024 - - - new logging / saving system that saves in subfolders. 
-    PROJECT_ROOT = '/Users/burnettl/Documents/Projects/oaky_cokey/'; %% Update for your computer. 
-    data_path = fullfile(PROJECT_ROOT, 'data');
-    results_path = fullfile(PROJECT_ROOT, 'results');
+    % PROJECT_ROOT = '/Users/burnettl/Documents/Projects/oaky_cokey/'; %% Update for your computer. 
+    ROOT_DIR = '/Users/burnettl/Documents/Projects/oaky_cokey/';
+    data_path = fullfile(ROOT_DIR, 'data');
+    results_path = fullfile(ROOT_DIR, 'results');
     
     date_folder = fullfile(data_path, date_to_analyse);
     
