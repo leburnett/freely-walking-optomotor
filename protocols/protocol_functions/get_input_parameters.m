58856caa4e79ab04bb81373acebbc89e6b037a46
leburnett <leburnett3@gmail.com>
2025-05-12 11:43:37 -0400

    add nFlies as dropdown option

--- protocols/protocol_functions/get_input_parameters.m
+++ protocols/protocol_functions/get_input_parameters.m
@@ -1,9 +1,16 @@
 function selections = get_input_parameters()
     
     % Create a figure window for the input dialog
-    fig = uifigure('Name', 'Fly Experiment Setup', 'Position', [500 500 300 300]);
+    fig = uifigure('Name', 'Fly Experiment Setup', 'Position', [500 500 300 350]);
 
-    % Dropdown for Landing site
+    % Dropdown for Number of Flies
+    uilabel(fig, 'Position', [20 300 80 22], 'Text', 'nFlies:');
+    nfliesDropdown = uidropdown(fig, ...
+        'Position', [120 300 150 22], ...
+        'Items', {'9', '10', '11', '12', '13', '14', '15', '16', '17'}, ...
+        'Value', '15');
+
+    % Dropdown for Experimenter
     uilabel(fig, 'Position', [20 250 80 22], 'Text', 'Experimenter:');
     experimenterDropdown = uidropdown(fig, ...
         'Position', [120 250 150 22], ...
@@ -14,7 +21,7 @@ function selections = get_input_parameters()
     uilabel(fig, 'Position', [20 200 80 22], 'Text', 'Fly Strain:');
     strainDropdown = uidropdown(fig, ...
         'Position', [120 200 150 22], ...
-        'Items', {'csw1118', 'ss324_t4t5_shibire_kir', 'jfrc100_es_shibire_kir', 'l1l4_jfrc100_shibire_kir', 'ss26283_H1_shibire_kir', 'ss01027_H2_shibire_kir', 'ss1209_DCH-VCH_shibire_kir', 'ss34318_Am1_shibire_kir', 'ss03722_Tm5Y_shibire_kir', 'ss00395_TmY3_shibire_kir','2575_LPC1_shibire_kir', 'ss02594_TmY5a_shibire_kir', 'ss02594_TmY5a_shibire_kirB', 't4t5_RNAi_control', 't4t5_mmd_RNAi', 't4t5_ttl_RNAi', 'test'}, ...
+        'Items', {'csw1118', 'ss324_t4t5_shibire_kir', 'jfrc100_es_shibire_kir', 'jfrc100_es_shibire_kirB','l1l4_jfrc100_shibire_kir', 'ss26283_H1_shibire_kir', 'ss01027_H2_shibire_kir', 'ss1209_DCH-VCH_shibire_kir', 'ss34318_Am1_shibire_kir', 'ss03722_Tm5Y_shibire_kir', 'ss00395_TmY3_shibire_kir', '2575_LPC1_shibire_kir', '2575_LPC1_shibire_kirB', 'ss02594_TmY5a_shibire_kir', 'ss02594_TmY5a_shibire_kirB', 'ss00297_Dm4_shibire_kir', 'ss00326_Pm2ab_shibire_kir', 't4t5_RNAi_control', 't4t5_mmd_RNAi', 't4t5_ttl_RNAi', 'test'}, ...
         'Value', 'jfrc100_es_shibire_kir');
 
     % Dropdown for Age
@@ -39,19 +46,20 @@ function selections = get_input_parameters()
         'Value', '01:00_17:00');
 
     % Variable to hold selections
-    selections = struct('Experimenter', '', 'Strain', '', 'Age', '', 'Sex', '', 'LightCycle', '');
+    selections = struct('nFlies', '', 'Experimenter', '', 'Strain', '', 'Age', '', 'Sex', '', 'LightCycle', '');
 
     % Confirm Button with callback function to retrieve values and close the figure
     confirmButton = uibutton(fig, 'push', ...
         'Position', [100 10 100 30], ...
         'Text', 'Confirm', ...
-        'ButtonPushedFcn', @(btn, event) confirmSelections(experimenterDropdown, strainDropdown, ageDropdown, sexDropdown, lightCycleDropdown, fig));
+        'ButtonPushedFcn', @(btn, event) confirmSelections(nfliesDropdown, experimenterDropdown, strainDropdown, ageDropdown, sexDropdown, lightCycleDropdown, fig));
 
     % Wait for the figure to close before proceeding
     uiwait(fig);
 
     % Callback function to store selections and close the figure
-    function confirmSelections(experimenterDropdown, strainDropdown, ageDropdown, sexDropdown, lightCycleDropdown, fig)
+    function confirmSelections(nfliesDropdown, experimenterDropdown, strainDropdown, ageDropdown, sexDropdown, lightCycleDropdown, fig)
+        selections.nFlies = nfliesDropdown.Value;
         selections.Experimenter = experimenterDropdown.Value;
         selections.Strain = strainDropdown.Value;
         selections.Age = ageDropdown.Value;
