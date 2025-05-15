2b0429fa8c28d2e05238c06f7fc02330a4126880
leburnett <leburnett3@gmail.com>
2025-05-15 10:30:51 -0400

    Update analyse_data_p30_p31.m

--- plotting_functions/analyse_data_p30_p31.m
+++ plotting_functions/analyse_data_p30_p31.m
@@ -6,6 +6,7 @@ cd(protocol_dir);
 
 DATA = comb_data_across_cohorts_cond(protocol_dir);
 
+%% 
 % strain = "jfrc100_es_shibire_kir";
 strain = "ss00297_Dm4_shibire_kir";
 data_types = {'av_data', 'fv_data', 'dist_data', 'dist_data_delta'};
@@ -16,7 +17,7 @@ for d = 1:4
     data_type = data_types{d};
     
     % f1 = plot_timeseries_diff_contrasts_1strain(DATA, strain, data_type, plot_sem);
-    
+    figure
     col = [1 0.5 0];
     f2 = plot_errorbar_tuning_curve_diff_contrasts(DATA, strain, col, data_type);
 end 
@@ -24,7 +25,7 @@ end
 
 %% Plot both strains over each other. 
 close
-data_type = "fv_data";
+data_type = "curv_data";
 
 strains = {"jfrc100_es_shibire_kir", "ss00297_Dm4_shibire_kir"};
 for st = [1,2]
@@ -55,13 +56,26 @@ for d = 1:4
 end 
 
 
-%% 
+%% Plot both strains on top of each other.
 close
-data_type = "dist_data_delta";
+data_type = "curv_data";
 strains = {"jfrc100_es_shibire_kir", "ss00297_Dm4_shibire_kir"};
 for st = [1,2]
 
     strain = strains{st};
     f3 = plot_errorbar_tuning_diff_speeds(DATA, strain, data_type);
-
 end 
+
+%% timeseries for different speed experiments
+
+strain = "jfrc100_es_shibire_kir";
+% strain = "ss00297_Dm4_shibire_kir";
+data_types = {'av_data', 'fv_data', 'dist_data', 'dist_data_delta', 'curv_data'};
+plot_sem = 0;
+
+for d = 1:5
+
+    data_type = data_types{d};
+    figure
+    f4 = plot_timeseries_diff_speeds(DATA, strain, data_type, plot_sem);
+end
