a0595274013c15e28860f54b9bfc2e0f63ff6b56
leburnett <leburnett3@gmail.com>
2025-05-19 16:22:08 -0400

    Update process_screen_p27_data.m

--- processing_functions/process_screen_p27_data.m
+++ processing_functions/process_screen_p27_data.m
@@ -1,6 +1,6 @@
-function process_screen_p27_data(ROOT_DIR)
+ function DATA = process_screen_p27_data()
 
-    % ROOT_DIR = '\Users\burnettl\Documents\oakey-cokey\';
+    ROOT_DIR = '/Users/burnettl/Documents/Projects/oaky_cokey';
     % Move to the directory to where the results per experiment are saved:
     protocol_dir = fullfile(ROOT_DIR, 'results', 'protocol_27');
     cd(protocol_dir);
@@ -16,6 +16,15 @@ function process_screen_p27_data(ROOT_DIR)
     strain_folders = dir;
     strain_folders = strain_folders([strain_folders.isdir]); % Keep only directories
     strain_folders = strain_folders(~ismember({strain_folders.name}, {'.', '..'})); % Remove '.' and '..'
+    
+    % Check for empty split folder - remove if it exists.
+    if ~ismember({strain_folders.name}, {'jfrc100_es_shibire_kir'})
+        disp("No data for empty split flies")
+    else 
+        strain_folders = strain_folders(~ismember({strain_folders.name}, {'jfrc100_es_shibire_kir'})); 
+    end 
+    
+    % Number of strains without ES.
     n_strains = height(strain_folders);
     
     % Check and change DCH VCH name. 
@@ -28,35 +37,22 @@ function process_screen_p27_data(ROOT_DIR)
     % Generate the struct 'DATA' that combines data across experiments and
     % separates data into conditions.
     DATA = comb_data_across_cohorts_cond(protocol_dir);
-    
-    % Which colour to plot each group in. 
-    gp_data = {
-        'jfrc100_es_shibire_kir', 'F', [0.7 0.7 0.7]; % light grey
-        'ss324_t4t5_shibire_kir', 'F', [0.6 0.8 0.6]; % green
-        'l1l4_jfrc100_shibire_kir', 'F', [0.4 0.8 1]; % blue
-        'ss26283_H1_shibire_kir', 'F', [0.8, 0 , 0]; % red
-        'ss01027_H2_shibire_kir', 'F', [0.8, 0.4, 0]; % orange
-        'ss1209_DCH_VCH_shibire_kir', 'F', [0.52, 0.12, 0.57]; % purple
-        'ss34318_Am1_shibire_kir', 'F', [1, 0.85, 0]; % gold
-        };
-    
+
     % Store figures in folders of the day that they were created too. Can go
     % back and look at previous versions. 
     date_str = string(datetime('now','TimeZone','local','Format','yyyy_MM_dd'));
     
     % If saving the figures - create a folder to save them in:
-    save_folder = fullfile(ROOT_DIR, "figures", protocol, date_str);
+    save_folder = fullfile(ROOT_DIR, "figures",  protocol, date_str);
     if ~isfolder(save_folder)
         mkdir(save_folder);
     end
-
+    
     % Save a text file with the number of flies and vials for each strain
     % that has been run so far: 
     exp_data = generate_exp_data_struct(DATA);
     export_num_flies_summary(exp_data, save_folder)
-    
-    %Save the groups that were used for the plots
-    writecell(gp_data, fullfile(save_folder,'group_data.txt'), 'Delimiter', ';')
+    generate_fly_n_bar_charts(exp_data, save_folder)
     
     cond_titles = {"60deg-gratings-4Hz"...
         , "60deg-gratings-8Hz"...
@@ -79,29 +75,39 @@ function process_screen_p27_data(ROOT_DIR)
     plot_sem = 1;
     data_types =  {'fv_data', 'av_data', 'curv_data', 'dist_data', 'dist_data_delta'};
     
-    for strain = 2:n_strains
+    for strain = 1:n_strains
     
-        grp_title = gp_data{strain, 1};
-        disp(strcat("Plotting the data for " , grp_title))
-    
-        for typ = 1:5
-    
-            % Data type to plot
-            data_type = data_types{typ};
-    
-            % Plot the chosen strain against the ES controls.
-            gps2plot = [1, strain];
-    
-            % Data in time series are downsampled by 10.
-            f_xgrp = plot_allcond_acrossgroups_tuning(DATA, gp_data, cond_titles, data_type, gps2plot, plot_sem);
+        grp_title = strain_folders(strain).name; 
+        gp_data = {
+            'jfrc100_es_shibire_kir', 'F', [0.7 0.7 0.7]; % light grey
+            grp_title, 'F', [0.52, 0.12, 0.57]; % purple
+            };
+
+        if isfield(DATA, grp_title) % Check if there is data for the strain in DATA
+            disp(strcat("Plotting the data for " , grp_title))
         
-            fname = fullfile(save_folder, strcat(grp_title, '_', data_type, ".pdf"));
-            exportgraphics(f_xgrp ...
-                , fname ...
-                , 'ContentType', 'vector' ...
-                , 'BackgroundColor', 'none' ...
-                ); 
-        end
+            for typ = 1:5
+        
+                % Data type to plot
+                data_type = data_types{typ};
+        
+                % Plot the chosen strain against the ES controls.
+                gps2plot = [1, 2];
+        
+                % Data in time series are downsampled by 10.
+                f_xgrp = plot_allcond_acrossgroups_tuning(DATA, gp_data, cond_titles, data_type, gps2plot, plot_sem);
+            
+                fname = fullfile(save_folder, strcat(grp_title, '_', data_type, ".pdf"));
+                exportgraphics(f_xgrp ...
+                    , fname ...
+                    , 'ContentType', 'vector' ...
+                    , 'BackgroundColor', 'none' ...
+                    ); 
+                close
+            end
+        else 
+            disp(strcat("No data for ", grp_title))
+        end 
     
     end 
 
