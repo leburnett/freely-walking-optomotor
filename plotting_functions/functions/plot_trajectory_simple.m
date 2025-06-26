348fa5d0a244f9d1ba0da83a663713dc679d1efe
leburnett <leburnett3@gmail.com>
2025-06-26 11:56:24 -0400

    Update plot_trajectory_simple.m

--- plotting_functions/functions/plot_trajectory_simple.m
+++ plotting_functions/functions/plot_trajectory_simple.m
@@ -2,8 +2,8 @@ function plot_trajectory_simple(x, y, cx, cy)
 % 'x' and 'y' are the x and y values of the fly over the period you would
 % like to plot the trajectory for. Arena centre is [cx, cy].
 
-    rectangle('Position',[1, 1, 245, 245], 'Curvature', [1,1], 'FaceColor', [0.95 0.95 0.95], 'EdgeColor', 'none')
-    viscircles([cx, cy], 121, 'Color', [0.8 0.8 0.8], 'LineStyle', '-', 'LineWidth', 0.8) % Edge
+    rectangle('Position',[0.25, 2.5, 245, 245], 'Curvature', [1,1], 'FaceColor', [0.95 0.95 0.95], 'EdgeColor', 'none')
+    viscircles([cx, cy], 121, 'Color', [0.8 0.8 0.8], 'LineStyle', '-', 'LineWidth', 1) % Edge
     viscircles([cx, cy], 110, 'Color', [0.8 0.8 0.8], 'LineStyle', '--', 'LineWidth',1) % 10mm from edge
     viscircles([cx, cy], 63, 'Color', [0.8 0.8 0.8], 'LineStyle', '--', 'LineWidth',1) % Half way
     hold on;
@@ -20,8 +20,8 @@ function plot_trajectory_simple(x, y, cx, cy)
     xlabel('X Position (mm)');
     ylabel('Y Position (mm)');
     axis equal;
-    xlim([0 246])
-    ylim([0 246])
+    xlim([-2 247])
+    ylim([0 248])
     legend;
 
     % f = gcf;
