0ff9fc3867ee5309c878b52c9721809fd53b14db
leburnett <leburnett3@gmail.com>
2025-07-02 14:36:47 -0400

    Update single_lady_analysis.m
    Include analysis / plots for only data from p25 - not bootstrapped data from p27.

--- processing_functions/single_lady_analysis.m
+++ processing_functions/single_lady_analysis.m
@@ -19,9 +19,11 @@ end
 
 save_fig = 0; 
 
+%% Method 1 - bootstrapping from p27 ES-Shibire-Kir flies.
+
 %% Create time series plots after bootstrapping the "grouped" ES data to have the same number of samples as the single ladies. 
 sex = 'F';
-cond_idx = 3;
+cond_idx = 1;
 
 data_types =  {'fv_data', 'av_data', 'curv_data', 'dist_data', 'dist_data_delta'};
 
@@ -129,5 +131,124 @@ for d = 1:numel(data_types)
 
 end 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+%% Method 2 - use grouped flies for p25. 
+
+load("/Users/burnettl/Documents/Projects/oaky_cokey/results/protocol_25/DATA_single_ladies_es_shibire_kir_p25.mat", 'DATA');
+
+
+%% Create time series plots after bootstrapping the "grouped" ES data to have the same number of samples as the single ladies. 
+strain = 'jfrc100_es_shibire_kir';
+cond_idx = 3;
+
+data_types =  {'fv_data', 'av_data', 'curv_data', 'dist_data', 'dist_data_delta'};
+
+for d = 1:numel(data_types)
+
+    data_type = data_types{d};
+
+    if data_type == "dist_data_delta"
+        data_type = "dist_data";
+        delta = 1;
+    else 
+        delta = 0;
+    end 
+
+    %% Get "cond_data" - array of size [number of flies x time points]
+    sex = 'G';
+    cond_data_grp = combine_timeseries_data_per_cond(DATA, strain, sex, data_type, cond_idx);
+    cond_data_grp = cond_data_grp(:, 1:1807);
+    
+    sex = 'F';
+    cond_data_solo = combine_timeseries_data_per_cond(DATA, strain, sex, data_type, cond_idx);
+    cond_data_solo = cond_data_solo(:, 1:1807);
+
+    if delta
+        cond_data_grp = cond_data_grp - cond_data_grp(:, 300);
+        cond_data_solo = cond_data_solo - cond_data_solo(:, 300);
+    end 
+    
+    % Parameters
+    n_flies = size(cond_data_solo, 1);  % Number of flies in solo condition
+    T = size(cond_data_solo, 2);   % Number of timepoints
+
+    % Compute mean and 95% CI across bootstrapped samples
+    group_mean = nanmean(cond_data_grp, 1);                   % [1 x T]
+    group_sem = nanstd(cond_data_grp, 0, 1) / sqrt(n_flies);
+    group_ci = [group_mean - 1.96 * group_sem;               % Lower 95% CI
+               group_mean + 1.96 * group_sem];              % Upper 95% CI  [2 x T]
+    
+    % Solo condition stats
+    solo_mean = nanmean(cond_data_solo, 1);                  % [1 x T]
+    solo_sem = nanstd(cond_data_solo, 0, 1) / sqrt(n_flies);
+    solo_ci = [solo_mean - 1.96 * solo_sem;               % Lower 95% CI
+               solo_mean + 1.96 * solo_sem];              % Upper 95% CI  [2 x T]
+    
+    %% Generate the plot
+    t = 1:T;  % or use your actual time vector here
+    
+    figure;
+    hold on;
+    
+    % Plot group mean with shaded 95% CI
+    fill([t, fliplr(t)], [group_ci(1,:), fliplr(group_ci(2,:))], ...
+         [0.8 0.8 0.8], 'EdgeColor', 'none', 'FaceAlpha', 0.4);
+    plot(t, group_mean, 'k', 'LineWidth', 2);
+    
+    % Plot solo mean with ±1 standard deviation
+    fill([t, fliplr(t)], [solo_ci(1,:), fliplr(solo_ci(2,:))], ...
+         [0.8 0.8 1], 'EdgeColor', 'none', 'FaceAlpha', 0.4);
+    plot(t, solo_mean, 'Color', [0 0 0.85], 'LineWidth', 2);
+    
+    ylb = get_ylb_from_data_type(data_type, delta);
+    
+    % Labels and legend
+    ylabel(ylb);
+    
+    ax = gca;
+    ax.XAxis.Visible = 'off';
+    ax.TickDir = 'out';
+    ax.LineWidth = 1.2;
+    ax.FontSize = 12;
+    
+    rng = ax.YLim;
+    
+    % Add vertical lines for the beginning, middle and end of the stimulus. 
+    plot([300 300], rng, 'Color', [0.7 0.7 0.7], 'LineWidth', 0.5)
+    plot([750 750], rng, 'Color', [0.7 0.7 0.7], 'LineWidth', 0.5) 
+    plot([1200 1200], rng, 'Color', [0.7 0.7 0.7], 'LineWidth', 0.5)  
+    
+    xlim([0 T])
+    
+    f = gcf;
+    f.Position = [284   752   475   260];
+    
+    title(cond_titles{cond_idx})
+    % legend({'Group 95% CI', 'Group Mean', 'Solo 95% CI', 'Solo Mean'});
+      
+    if save_fig
+        if delta 
+            data_type = "dist_data_delta";
+        end
+        fname = fullfile(fig_date_folder, strcat("p25_", string(n_samples), "_Cond", string(cond_idx),"_", cond_titles{cond_idx},"_", data_type, ".png"));
+        exportgraphics(f, fname); 
+    end 
+
+end 
 
 end 
