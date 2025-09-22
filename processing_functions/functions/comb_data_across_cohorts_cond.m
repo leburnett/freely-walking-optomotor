d0c16d78009f7db1e3d6e4143a0b81f1048462d7
leburnett <leburnett3@gmail.com>
2025-09-22 07:36:48 -0400

    Update comb_data_across_cohorts_cond.m

--- processing_functions/functions/comb_data_across_cohorts_cond.m
+++ processing_functions/functions/comb_data_across_cohorts_cond.m
@@ -1,121 +1,31 @@
 function DATA = comb_data_across_cohorts_cond(protocol_dir)
-    % Example 'protocol_dir' would be:
-    % '/Users/burnettl/Documents/Projects/oaky_cokey/results/protocol_10'
+    % Combine the data within "comb_data" - behavioural data for each
+    % individual fly across a single cohort for one run of an experiment 
+    % - across all experiments for a particular protocol.
 
-    % NOTE: a separate function exists for processing protocol 14. This
-    % function works for all protocols that have been generated in the
-    % style of protocol 10 with a random condition structure.
-    
-    % [optomotor_pattern, flicker_pattern, opto_speed, flick_speed, trial_len]
+    % This function has now been optimised for "protocol_27" - the screen
+    % protocol. 
 
-    strs = split(protocol_dir, '/');
-    protocol = strs{end};
+    % Inputs:
+    % _______
 
-    if protocol == "protocol_10"
-        cond_array =[9, 10, 64, 8, 2;
-            9, 10, 127, 16, 15;
-            9, 10, 64, 8, 15;
-            9, 10, 127, 16, 2;
-            6, 7, 64, 8, 2;
-            6, 7, 127, 16, 15;
-            6, 7, 64, 8, 15;
-            6, 7, 127, 16, 2;
-            4, 5, 64, 8, 2;
-            4, 5, 127, 16, 15;
-            4, 5, 64, 8, 15;
-            4, 5, 127, 16, 2;
-            ];
-    elseif protocol == "protocol_15"
-        cond_array = [
-            4, 5, 64, 8, 15; % standard 4ON 4OFF
-            13, 14, 64, 8, 15; % 4ON 12OFF
-            16, 14, 64, 8, 15; % 12 ON 4OFF
-            ];
-    elseif protocol == "protocol_18"
-        cond_array = [
-            9, 10, 127, 8, 15, 1; % Normal gratings
-            9, 10, 64, 8, 15, 2;
-            19, 10, 64, 8, 15, 3; % ON curtain
-            19, 10, 127, 8, 15, 4;
-            20, 10, 64, 8, 15, 5; % OFF curtain
-            20, 10, 127, 8, 15, 6;
-        ];
-    % elseif protocol == "protocol_19" % BEFORE JAN 2025
-    %     cond_array =  [
-    %         9, 10, 127, 8, 15, 1; % normal stripes - wide
-    %         9, 10, 64, 8, 15, 2;
-    %         19, 10, 64, 8, 15, 3; % ON curtain
-    %         19, 10, 127, 8, 15, 4;
-    %         20, 10, 64, 8, 15, 5; % OFF curtain
-    %         20, 10, 127, 8, 15, 6;
-    %         17, 18, 64, 8, 15, 7; % 2ON 14OFF grating
-    %         17, 18, 127, 8, 15, 8;
-    %     ];
-    elseif protocol == "protocol_19" % AFTER JAN 2025
-        % cond_array =  [
-        %     9, 10, 64, 8, 15, 1; % normal stripes - wide
-        %     9, 10, 127, 8, 15, 2;
-        %     19, 10, 64, 8, 15, 3; % ON curtain
-        %     19, 10, 127, 8, 15, 4;
-        %     20, 10, 64, 8, 15, 5; % OFF curtain
-        %     20, 10, 127, 8, 15, 6;
-        %     17, 10, 64, 8, 15, 7; % 2ON 14OFF grating
-        %     17, 10, 127, 8, 15, 8;
-        %     24, 10, 64, 8, 15, 9; % 2OFF 14ON grating
-        %     24, 10, 127, 8, 15, 10;
-        % ];
-        cond_array = [  % From 27th Jan onwards - added thin gratings back in. 
-            9, 10, 64, 8, 15, 1; % grating - 60 deg
-            9, 10, 127, 8, 15, 2;
-            19, 10, 64, 8, 15, 3; % ON curtain - - 5
-            19, 10, 127, 8, 15, 4; % - - 6
-            20, 10, 64, 8, 15, 5; % OFF curtain - - 7
-            20, 10, 127, 8, 15, 6; % - - 8
-            17, 10, 64, 8, 15, 7; % 2ON 14OFF grating - - 9
-            17, 10, 127, 8, 15, 8; % - - 10
-            24, 10, 64, 8, 15, 9; % 2OFF 14ON grating - - 11
-            24, 10, 127, 8, 15, 10; %  - - 12
-            4, 10, 64, 8, 15, 11; % grating - 15 deg - - 3
-            4, 10, 127, 8, 15, 12; % - - 4
-            ];
-    elseif protocol == "protocol_21"
-        cond_array = [ 
-                27, 29, 64, 1, 15, 1; % grating - 60 deg
-                27, 29, 127, 1, 15, 2;
-                26, 29, 64, 1, 15, 3; % grating - 30 deg - - 3
-                26, 29, 127, 1, 15, 4; % - - 4
-                19, 29, 16, 1, 15, 5; % ON curtain - - 5
-                19, 29, 32, 1, 15, 6; % - - 6
-                20, 29, 16, 1, 15, 7; % OFF curtain - - 7
-                20, 29, 32, 1, 15, 8; % - - 8
-                10, 29, 4, 1, 15, 9; % Flicker as a stimulus - 60 deg - slow
-                10, 29, 8, 1, 15, 10; % Flicker as a stimulus - 60 deg - fast
-            ]; 
-    elseif protocol == "protocol_22"
-        cond_array = [ 
-            30, 29, 1, 1, 15, 1; % 1 = bar fixation
-            31, 29, 32, 1, 15, 2; % reverse phi - 1px step
-            32, 29, 32, 1, 15, 3; % 4 px step
-            33, 29, 32, 1, 15, 4; % 8 px step
-            34, 29, 32, 1, 15, 5; % FoE - 30deg
-            35, 29, 32, 1, 15, 6; % 15 deg
-            36, 29, 32, 1, 15, 7; % 60 deg
-        ]; 
-    elseif protocol == "protocol_23"
-        cond_array = [ 
-            45, 9, 1, 127, 60, 7; % 1 = bar fixation
-            46, 9, 1, 127, 60, 8; % 2 = bar fixation
-        ]; 
-    end 
+    % 'protocol_dir': path
+    %       Path to the results files for a particular protocol. E.g. 
+    % '/Users/burnettl/Documents/Projects/oaky_cokey/results/protocol_27'
+
+    % Returns:
+    % _______
+
+    % DATA : struct
+    %       MATLAB struct that contains the data from multiple cohorts
+    %       parsed based on data type and condition.
 
     % Find all processed data for one protocol. 
     filelist = dir(fullfile(protocol_dir, '**/*.mat'));
+
     % Remove the DATA file if it already exits:
     idxToRemove = contains({filelist.name}, "DATA");
     filelist(idxToRemove) = [];
-
-    str_spl = split(protocol_dir, '/');
-    protocol_name = str_spl{end};
 
     n_files = length(filelist);
     
@@ -128,12 +38,9 @@ function DATA = comb_data_across_cohorts_cond(protocol_dir)
         disp(fname)
         f_folder = filelist(idx).folder; 
     
-        % Load 'combined_data', 'LOG', 'feat', 'trx' and 'n_fly_data'
-        load(fullfile(f_folder, fname));
-
-        if protocol == "protocol_24" || protocol == "protocol_27"
-            cond_array = LOG.meta.cond_array;
-        end 
+        % Load 'comb_data', 'LOG', and 'n_fly_data' that was generated by
+        % `combine_data_one_cohort` within `process_data_features`.
+        load(fullfile(f_folder, fname), 'comb_data', 'LOG', 'n_fly_data');
 
         % Get key information about strain and sex:
         strain = LOG.meta.fly_strain;
@@ -211,7 +118,7 @@ function DATA = comb_data_across_cohorts_cond(protocol_dir)
         logfields = fields(startsWith(fields, 'log_'));
         n_cond = height(logfields);
 
-        %% Then run through the next 16 logs. 
+        %% Then run through the conditions: 
         for log_n = 1:n_cond
     
             Log = LOG.(logfields{log_n});
@@ -260,7 +167,8 @@ function DATA = comb_data_across_cohorts_cond(protocol_dir)
         %% Add data from acclim_off2
         Log = LOG.acclim_off2;
         start_f = Log.start_f(1);
-        stop_f = Log.stop_f(end);
+        % stop_f = Log.stop_f(end); % Could use the actual last frame
+        % instead of "end".
         DATA.(strain).(sex)(sz).acclim_off2.vel_data = comb_data.vel_data(:, start_f:end);
         DATA.(strain).(sex)(sz).acclim_off2.fv_data = comb_data.fv_data(:, start_f:end);
         DATA.(strain).(sex)(sz).acclim_off2.dist_data = comb_data.dist_data(:, start_f:end);
@@ -277,6 +185,8 @@ function DATA = comb_data_across_cohorts_cond(protocol_dir)
     
     end 
 
+    % Save 'DATA' - this is currently commented out because the struct were
+    % becoming > 2GB and so couldn't be saved.
     % todaysdate =  string(datetime('now', 'Format','yyyy-MM-dd'));
     % save(string(fullfile(protocol_dir, strcat(protocol, '_DATA_', todaysdate, '.mat'))), 'DATA');
 
