23ea337b9269fb1808c1e4fb52e41160936580c3
leburnett <leburnett3@gmail.com>
2025-12-12 16:56:29 -0500

    Fig 1 plots

---
+++ plotting_functions/figures/fig1_plots.m
@@ -0,0 +1,189 @@
+
+%% Make plots for MIC paper - time series
+
+%% 1 - make DATA
+
+ROOT_DIR = '/Users/burnettl/Documents/Projects/oaky_cokey';
+% Move to the directory to where the results per experiment are saved:
+protocol_dir = fullfile(ROOT_DIR, 'results', 'protocol_27');
+cd(protocol_dir);
+
+% Get all of the strain folders that are inside the protocol folder.
+strain_folders = dir;
+strain_folders = strain_folders([strain_folders.isdir]); % Keep only directories
+strain_folders = strain_folders(~ismember({strain_folders.name}, {'.', '..'})); % Remove '.' and '..'
+
+% Check for empty split folder - remove if it exists.
+if ~ismember({strain_folders.name}, {'jfrc100_es_shibire_kir'})
+    disp("No data for empty split flies")
+else 
+    strain_folders = strain_folders(~ismember({strain_folders.name}, {'jfrc100_es_shibire_kir'})); 
+end 
+
+% Number of strains without ES.
+n_strains = height(strain_folders);
+
+% Check and change DCH VCH name. 
+folderMatch = any(contains({strain_folders.name}, "ss1209_DCH-VCH_shibire_kir"));
+if folderMatch
+    index = find(contains({strain_folders.name}, "ss1209_DCH-VCH_shibire_kir"));
+    strain_folders(index).name = strrep(strain_folders(index).name, "ss1209_DCH-VCH_shibire_kir", "ss1209_DCH_VCH_shibire_kir");
+end 
+
+% Generate the struct 'DATA' that combines data across experiments and
+% separates data into conditions.
+DATA = comb_data_across_cohorts_cond(protocol_dir);
+
+
+% % Store figures in folders of the day that they were created too. Can go
+% % back and look at previous versions. 
+% date_str = string(datetime('now','TimeZone','local','Format','yyyy_MM_dd'));
+% 
+% % If saving the figures - create a folder to save them in:
+% save_folder = fullfile(ROOT_DIR, "figures", protocol, date_str);
+% if ~isfolder(save_folder)
+%     mkdir(save_folder);
+% end
+
+cond_titles = {"60deg-gratings-4Hz"...
+            , "60deg-gratings-8Hz"...
+            , "narrow-ON-bars-4Hz"...
+            , "narrow-OFF-bars-4Hz"...
+            , "ON-curtains-8Hz"...
+            , "OFF-curtains-8Hz"...
+            , "reverse-phi-2Hz"...
+            , "reverse-phi-4Hz"...
+            , "60deg-flicker-4Hz"...
+            , "60deg-gratings-static"...
+            , "60deg-gratings-0-8-offset"...
+            , "32px-ON-single-bar"...
+            };
+
+% Blues
+% col_12 = [31 120 180; ... %50 50 50; ...% 166 206 227; 106 61 154; ...
+%     31 120 180; ...
+%     178 223 138; ...
+%     47 141 41; ...
+%     251 154 153; ...
+%     227 26 28; ...
+%     253 191 111; ...
+%     255 127 0; ...
+%     166 206 227; ...%202 178 214; ...
+%     200 200 200; ... % 106 61 154; ...166 206 227;
+%     255 224 41; ...
+%     187 75 12; ...
+%     ]./255;
+
+% Blue colours used: 
+% 4 Hz  -  31 120 180;
+% Flicker - 166 206 227;
+% Static - 200 200 200;
+
+%% 2 - Time series plot - just ES flies 
+
+figure_folder = '/Users/burnettl/Documents/Projects/oaky_cokey/figures/FIGS';
+
+
+strain = "jfrc100_es_shibire_kir";
+fps = 30; 
+sex = "F";
+data = DATA.(strain).(sex);
+cond_n = 1; 
+plot_sem = 1;
+data_types =  {'fv_data', 'av_data', 'curv_data', 'dist_data', 'dist_data_delta',};
+
+% Convert names - dashes to underscore.
+for i = 1:numel(strain_folders)
+    if isfield(strain_folders(i), 'name') && ischar(strain_folders(i).name) || isstring(strain_folders(i).name)
+        strain_folders(i).name = strrep(strain_folders(i).name, '-', '_');
+    end
+end
+
+% PLOT 
+strain_names = strain;
+cond_ids = [10, 9, 1];
+data_type = "fv_data";
+protocol = "protocol_27";
+params.save_figs = 0;
+params.plot_sem = 1;
+params.plot_sd = 0;
+params.plot_individ = 0;
+params.shaded_areas = 0;
+
+figure;
+plot_xcond_per_strain2(protocol, data_type, cond_ids, strain_names, params, DATA)
+f = gcf;
+f.Position = [181   611   641   340];
+
+if params.plot_sem == 1
+    sd_str = "SEM";
+elseif params.plot_sd == 1
+    sd_str = "SD";
+end 
+
+savefig(fullfile(figure_folder, strcat('TimeSeries_p27_Cond10-9-1_ES_', sd_str,'_', data_type,'.fig')));
+
+
+%% Plot JUST 4Hz responses with shaded areas showing the time over which metrics were calculated. 
+figure_folder = '/Users/burnettl/Documents/Projects/oaky_cokey/figures/FIGS/ES_timeseries_with_shaded_metric_areas';
+
+strain_names = strain;
+cond_ids = 1;
+data_type = "dist_data";
+protocol = "protocol_27";
+params.save_figs = 0;
+params.plot_sem = 1;
+params.plot_sd = 0;
+params.plot_individ = 0;
+params.shaded_areas = 1;
+
+figure;
+plot_xcond_per_strain2(protocol, data_type, cond_ids, strain_names, params, DATA)
+f = gcf;
+f.Position = [181   611   641   340];
+
+if params.plot_sem == 1
+    sd_str = "SEM";
+elseif params.plot_sd == 1
+    sd_str = "SD";
+end 
+
+savefig(fullfile(figure_folder, strcat('TimeSeries_p27_Cond10-9-1_ES_', sd_str,'_', data_type,'.fig')));
+
+
+% FV
+% 300:1200
+% 210:300 - 300:390 (3s +/-)
+
+% AV 
+% 300:1200
+% 315:450 (first 5s)
+
+% DIST1
+% 270:300 (1s around start)
+% 1170:1200 (1s around end)
+
+% DIST2 
+% 570:600 (1s min - dist moved from start to 10s in)
+% 1170:1200 
+
+% DIST3
+% 1470:1500 (difference from end of stim to 10s after stim ends)
+
+
+
+
+
+
+
+
+
+
+%% 3 - Box and whisker plots. 
+
+
+
+
+
+
+
--- plotting_functions/functions/plot_xcond_per_strain2.m
+++ plotting_functions/functions/plot_xcond_per_strain2.m
@@ -24,8 +24,24 @@ function plot_xcond_per_strain2(protocol, data_type, cond_ids, strain_names, par
 
     xmax = 1800;
 
+    % Purples
     % Colourmap:
-    col_12 = [166 206 227; ...
+    % col_12 = [106 61 154; ... %50 50 50; ...% 166 206 227; 106 61 154; ...
+    %     31 120 180; ...
+    %     178 223 138; ...
+    %     47 141 41; ...
+    %     251 154 153; ...
+    %     227 26 28; ...
+    %     253 191 111; ...
+    %     255 127 0; ...
+    %     202 178 214; ...
+    %     150 150 150; ... % 106 61 154; ...166 206 227;
+    %     255 224 41; ...
+    %     187 75 12; ...
+    %     ]./255;
+
+    % Blues
+    col_12 = [31 120 180; ... %50 50 50; ...% 166 206 227; 106 61 154; ...
         31 120 180; ...
         178 223 138; ...
         47 141 41; ...
@@ -33,21 +49,38 @@ function plot_xcond_per_strain2(protocol, data_type, cond_ids, strain_names, par
         227 26 28; ...
         253 191 111; ...
         255 127 0; ...
-        202 178 214; ...
-        106 61 154; ...
+        166 206 227; ...%202 178 214; ...
+        200 200 200; ... % 106 61 154; ...166 206 227;
         255 224 41; ...
         187 75 12; ...
         ]./255;
 
         
+if params.plot_sem == 1
     if data_type == "fv_data" 
-        rng = [0 20];
+        rng = [0 15];
     elseif data_type == "dist_data_delta"
         delta = 1;
         data_type = "dist_data";
-        rng = [-40 15];
+        rng = [-45 10];
     elseif data_type == "dist_data"
-        rng = [20 100];
+        rng = [30 90];
+    elseif data_type == "view_dist"
+        rng = [60 140];
+    elseif data_type == "dist_dt"
+        rng = [-7 5];
+    elseif data_type == "av_data" || data_type == "curv_data" 
+        rng = [-200 200];
+    end 
+elseif params.plot_sd == 1
+    if data_type == "fv_data" 
+        rng = [0 22];
+    elseif data_type == "dist_data_delta"
+        delta = 1;
+        data_type = "dist_data";
+        rng = [-70 30];
+    elseif data_type == "dist_data"
+        rng = [0 120];
     elseif data_type == "view_dist"
         rng = [60 140];
     elseif data_type == "dist_dt"
@@ -55,134 +88,194 @@ function plot_xcond_per_strain2(protocol, data_type, cond_ids, strain_names, par
     elseif data_type == "av_data" || data_type == "curv_data" 
         rng = [-350 350];
     end 
+end 
     
-    %% PLOT 
-   
-    for strain_id = 1:numel(strain_names)
-    
-        strain = strain_names{strain_id};
-        
-        % figure
-        for c = 1:numel(cond_ids)
-        
-            condition_n = cond_ids(c);
-            col = col_12(c, :);
-        
-            data = DATA.(strain).(sex);
-            cond_data = combine_timeseries_across_exp(data, condition_n, data_type); % absolute
-            if delta
-                cond_data = cond_data - cond_data(:, 300); % relative
-            end 
-        
-            mean_data = nanmean(cond_data);
-            % sem_data = nanstd(cond_data)/sqrt(size(cond_data,1));
-            sem_data = nanstd(cond_data);
-        
-            y1 = mean_data+sem_data;
-            y2 = mean_data-sem_data;
-            nf_comb = size(mean_data, 2);
-            x = 1:1:nf_comb;
+%% PLOT 
 
-            if params.plot_individ == 1
-                n_indiv = height(cond_data);
-                for id = 1:n_indiv
-                    plot(cond_data(id, :), 'Color', [0.7 0.7 0.7], 'LineWidth', 0.7); 
-                    hold on
-                end 
-            end 
-        
-            if params.plot_sem == 1
-                plot(x, y1, 'w', 'LineWidth', 1)
-                hold on
-                plot(x, y2, 'w', 'LineWidth', 1)
-                patch([x fliplr(x)], [y1 fliplr(y2)], col, 'FaceAlpha', 0.4, 'EdgeColor', 'none')
-            end
-            plot(mean_data, 'Color', col, 'LineWidth', 2.5);
-            hold on
+hold on
+if params.shaded_areas == 1
+    if data_type == "fv_data" 
+
+        % Full stim
+        rectangle('Position', [300, rng(1), 900, rng(2)], ...
+              'FaceColor', [0.7 0.7 0.7], 'EdgeColor', 'none', 'FaceAlpha', 0.3);
+
+        % % Change at start
+        % rectangle('Position', [210, rng(1), 90, rng(2)], ...
+        %       'FaceColor', [1 0.4 0.4], 'EdgeColor', 'none', 'FaceAlpha', 0.3);
+        % 
+        % 
+        % rectangle('Position', [300, rng(1), 90, rng(2)], ...
+        %       'FaceColor', [0.4 0.4 1], 'EdgeColor', 'none', 'FaceAlpha', 0.3);
+
+    elseif data_type == "dist_data"
+
+        if delta == 1
+
+            % within 10s
+            rectangle('Position', [570, rng(1), 30, diff(rng)], ...
+                  'FaceColor', [0.7 0.7 0.7], 'EdgeColor', 'none', 'FaceAlpha', 0.3);
+
+            % end of stim
+            rectangle('Position', [1170, rng(1), 30, diff(rng)], ...
+                  'FaceColor', [0.7 0.7 0.7], 'EdgeColor', 'none', 'FaceAlpha', 0.3);
+        else
+
+            % 1s start 
+            rectangle('Position', [270, rng(1), 30, diff(rng)], ...
+                  'FaceColor', [0.7 0.7 0.7], 'EdgeColor', 'none', 'FaceAlpha', 0.3);
+    
+            % 1s end
+            rectangle('Position', [1170, rng(1), 30, diff(rng)], ...
+                  'FaceColor', [0.7 0.7 0.7], 'EdgeColor', 'none', 'FaceAlpha', 0.3);
+
+            % Diff stop to int
+            rectangle('Position', [1198, rng(1), 4, diff(rng)], ...
+                  'FaceColor', [1 0.4 0.4], 'EdgeColor', 'none', 'FaceAlpha', 0.9);
+            rectangle('Position', [1470, rng(1), 30, diff(rng)], ...
+                  'FaceColor', [0.4 0.4 1], 'EdgeColor', 'none', 'FaceAlpha', 0.3);
         end 
-        
-        box off
-        ax = gca;
-        ax.TickDir = 'out';
-        ax.LineWidth = 1.2;
-        ax.FontSize = 14;
-        
-        f = gcf;
-        % f.Position = [233   581   603   390]; % p27
-        f.Position = [233   511   641   460]; % p31
-        
-        ylb = get_ylb_from_data_type(data_type, delta);
-        ylabel(ylb)
-        
-        xticks([0, 300, 600, 900, 1200, 1500, 1800, 2100, 2400])
-        xticklabels({'-10','0', '10', '20', '30', '40', '50', '60', '70'})
-        xlabel('Time (s)')
 
-        % Add vertical lines for start, middle and end of stim.
-        plot([300 300], rng, 'Color', [0.7 0.7 0.7], 'LineWidth', 0.5)
-        plot([750 750], rng, 'Color', [0.7 0.7 0.7], 'LineWidth', 0.5) % beginning of stim
-        plot([1200 1200], rng, 'Color', [0.7 0.7 0.7], 'LineWidth', 0.5) % change of direction   
-        
-        plot([0 xmax], [0 0], 'Color', [0.7 0.7 0.7], 'LineWidth', 0.5) 
-            
-        % title(strrep(strain, '_', '-'))
-    
-        ylim(rng)
-        xlim([0 xmax])
+    elseif data_type == "av_data" || data_type == "curv_data" 
 
-        %% RECTANGLES AT TOP
-        % Determine current y-axis range and add a band of rectangles above the data.
-        yl = ylim;                    % [ymin ymax] after setting ylim(rng)
-        ymin = yl(1);
-        ymax = yl(2);
-        yrange = ymax - ymin;
-        
-        rect_h = yrange / 20;         % height is 1/20 of current y-range
-        
-        % Extend y-limits to make space for the rectangles
-        ylim([ymin ymax + rect_h]);
-        
-        % y position where rectangles will start (just above the old ymax)
-        rect_y = ymax;
-        
-        % ==== First rectangle: 0 to 300 ====
-        rectangle('Position', [0, rect_y, 300, rect_h], ...
-                  'FaceColor', [0.4 0.4 0.4], 'EdgeColor', 'k');
-        
-        % ==== Middle region: bars from 300 to 1200 ====
-        bar_width = 15;
-        x_start = 300;
-        x_end = 1200;
-        
-        x_positions = x_start:bar_width:(x_end - bar_width);
-        
-        for i = 1:length(x_positions)
-            x0 = x_positions(i);
-        
-            % alternate colors: black, white, black, ...
-            if mod(i,2)==1
-                fc = 'w';     % odd → white
-            else
-                fc = 'k';     % even → black
-            end
-        
-            rectangle('Position', [x0, rect_y, bar_width, rect_h], ...
-                      'FaceColor', fc, ...
-                      'EdgeColor', 'k');
-        end
-        
-        % ==== Final rectangle: 1200 to xmax ====
-        rectangle('Position', [1200, rect_y, xmax-1200, rect_h], ...
-                  'FaceColor', [0.4 0.4 0.4], 'EdgeColor', 'k');
+        % Full stim
+        % rectangle('Position', [300, rng(1), 900, diff(rng)], ...
+        %       'FaceColor', [0.7 0.7 0.7], 'EdgeColor', 'none', 'FaceAlpha', 0.3);
 
-        
-        if params.save_figs == 1
-            % Save the figure. 
-            f_str = strcat("XCond_timeseries_", strain, "_", data_type, ".png");
-            fname = fullfile(save_folder, f_str);
-            exportgraphics(f, fname); 
-            close
-        end 
-    
+        % % First 5s - slight delay
+        rectangle('Position', [315, rng(1), 150, diff(rng)], ...
+              'FaceColor', [0.7 0.7 0.7], 'EdgeColor', 'none', 'FaceAlpha', 0.3);
     end 
+end 
+   
+for strain_id = 1:numel(strain_names)
+
+    strain = strain_names{strain_id};
+    
+    for c = 1:numel(cond_ids)
+    
+        condition_n = cond_ids(c);
+        col = col_12(condition_n, :);
+    
+        data = DATA.(strain).(sex);
+        cond_data = combine_timeseries_across_exp(data, condition_n, data_type); % absolute
+        if delta
+            cond_data = cond_data - cond_data(:, 300); % relative
+        end 
+    
+        mean_data = nanmean(cond_data);
+
+        if params.plot_sem == 1
+            sem_data = nanstd(cond_data)/sqrt(size(cond_data,1));
+        elseif params.plot_sd == 1
+            sem_data = nanstd(cond_data);
+        end 
+
+        y1 = mean_data+sem_data;
+        y2 = mean_data-sem_data;
+        nf_comb = size(mean_data, 2);
+        x = 1:1:nf_comb;
+
+        if params.plot_individ == 1
+            n_indiv = height(cond_data);
+            for id = 1:n_indiv
+                plot(cond_data(id, :), 'Color', [0.7 0.7 0.7], 'LineWidth', 0.7); 
+                hold on
+            end 
+        end 
+    
+        if params.plot_sem == 1 || params.plot_sd == 1
+            plot(x, y1, 'w', 'LineWidth', 1)
+            hold on
+            plot(x, y2, 'w', 'LineWidth', 1)
+            patch([x fliplr(x)], [y1 fliplr(y2)], col, 'FaceAlpha', 0.25, 'EdgeColor', 'none')
+        end
+        plot(mean_data, 'Color', col, 'LineWidth', 2.5);
+        hold on
+    end 
+    
+    box off
+    ax = gca;
+    ax.TickDir = 'out';
+    ax.LineWidth = 1.2;
+    ax.FontSize = 14;
+    
+    f = gcf;
+    % f.Position = [233   581   603   390]; % p27
+    f.Position = [233   511   641   460]; % p31
+    
+    ylb = get_ylb_from_data_type(data_type, delta);
+    ylabel(ylb)
+    
+    xticks([0, 300, 600, 900, 1200, 1500, 1800, 2100, 2400])
+    xticklabels({'-10','0', '10', '20', '30', '40', '50', '60', '70'})
+    xlabel('Time (s)')
+
+    % Add vertical lines for start, middle and end of stim.
+    plot([300 300], rng, 'Color', [0.7 0.7 0.7], 'LineWidth', 0.5)
+    plot([750 750], rng, 'Color', [0.7 0.7 0.7], 'LineWidth', 0.5) % beginning of stim
+    plot([1200 1200], rng, 'Color', [0.7 0.7 0.7], 'LineWidth', 0.5) % change of direction   
+    
+    plot([0 xmax], [0 0], 'Color', [0.7 0.7 0.7], 'LineWidth', 0.5) 
+        
+    % title(strrep(strain, '_', '-'))
+
+    ylim(rng)
+    xlim([0 xmax])
+
+    %% RECTANGLES AT TOP
+    % Determine current y-axis range and add a band of rectangles above the data.
+    yl = ylim;                    % [ymin ymax] after setting ylim(rng)
+    ymin = yl(1);
+    ymax = yl(2);
+    yrange = ymax - ymin;
+    
+    rect_h = yrange / 20;         % height is 1/20 of current y-range
+    
+    % Extend y-limits to make space for the rectangles
+    ylim([ymin ymax + rect_h]);
+    
+    % y position where rectangles will start (just above the old ymax)
+    rect_y = ymax;
+    
+    % ==== First rectangle: 0 to 300 ====
+    rectangle('Position', [0, rect_y, 300, rect_h], ...
+              'FaceColor', [0.4 0.4 0.4], 'EdgeColor', 'k');
+    
+    % ==== Middle region: bars from 300 to 1200 ====
+    bar_width = 15;
+    x_start = 300;
+    x_end = 1200;
+    
+    x_positions = x_start:bar_width:(x_end - bar_width);
+    
+    for i = 1:length(x_positions)
+        x0 = x_positions(i);
+    
+        % alternate colors: black, white, black, ...
+        if mod(i,2)==1
+            fc = 'w';     % odd → white
+        else
+            fc = 'k';     % even → black
+        end
+    
+        rectangle('Position', [x0, rect_y, bar_width, rect_h], ...
+                  'FaceColor', fc, ...
+                  'EdgeColor', 'k');
+    end
+    
+    % ==== Final rectangle: 1200 to xmax ====
+    rectangle('Position', [1200, rect_y, xmax-1200, rect_h], ...
+              'FaceColor', [0.4 0.4 0.4], 'EdgeColor', 'k');
+
+    
+    if params.save_figs == 1
+        % Save the figure. 
+        f_str = strcat("XCond_timeseries_", strain, "_", data_type, ".png");
+        fname = fullfile(save_folder, f_str);
+        exportgraphics(f, fname); 
+        close
+    end 
+
+end 
 end 
